4326981cc7ddf22dfc1f9254997fc503cb4a3eff
Andrey Pshenitsyn <include5691@gmail.com>
2024-11-21 19:18:18 +0500

    update[orm/user]: add timestamp field

--- e5lib/orm/user.py
+++ e5lib/orm/user.py
@@ -1,15 +1,18 @@
+import time
 from sqlalchemy import Column, Integer, String
 
 class User:
     __tablename__ = 'users'
     
     id = Column(Integer, primary_key=True)
+    timestamp = Column(Integer, nullable=False)
     name = Column(String(50))
     dep_id = Column(Integer)
     dep_name = Column(String(50))
 
-    def __init__(self, id, name, dep_id, dep_name):
+    def __init__(self, id: str | int, name: str, dep_id: str | int, dep_name: str, timestamp: int | float = time.time()):
         self.id = id
+        self.timestamp = int(timestamp)
         if name:
             self.name = name[:50]
         self.dep_id = dep_id
