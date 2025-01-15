cc7cd62da360e5cba4ce10c544c3214de70d5081
Andrey Pshenitsyn <include5691@gmail.com>
2025-01-15 22:13:43 +0500

    update[readme]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='e5lib',
     version='0.1.0',
     author='Andrey Pshenitsyn',
-    description='Bitrix24 rest api automatization utils library',
+    description='Utils library',
     packages=find_packages(),
     install_requires=[
         'sqlalchemy',
