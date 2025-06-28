ae3782685d4479dbc80a7f9494409d8e749dd051
Andrey Pshenitsyn <include5691@gmail.com>
2025-06-28 22:42:28 +0500

    feat(orm/connections): add pool_size arg to get_engine func

--- e5lib/orm/connections.py
+++ e5lib/orm/connections.py
@@ -5,12 +5,17 @@ from sqlalchemy.orm import sessionmaker
 engine_ = None
 sessionmaker_ = None
 
-def get_engine() -> Engine:
+
+def get_engine(pool_size: int | None = None) -> Engine:
     """Create sqlalchemy engine or return existing"""
     global engine_
     if not engine_:
-        engine_ = create_engine(os.getenv("DB_AUTH_URL"), pool_pre_ping=True)
+        kwargs = {"pool_pre_ping": True}
+        if pool_size is not None:
+            kwargs["pool_size"] = pool_size
+        engine_ = create_engine(os.getenv("DB_AUTH_URL"), **kwargs)
     return engine_
+
 
 def get_sessionmaker() -> sessionmaker:
     """Create sqlalchemy SessionMaker or return existing"""
