df380b0b0a1b3633c7a882beefd093e4d61dbd98
Andrey Pshenitsyn <include5691@gmail.com>
2025-07-01 12:38:23 +0500

    feat: add b24 module

---
+++ e5lib/b24/__init__.py
@@ -0,0 +1,1 @@
+from .users import get_dep_ids, get_dep_id, is_fired, is_in_prohibited_department
---
+++ e5lib/b24/users.py
@@ -0,0 +1,50 @@
+import os
+import logging
+from ast import literal_eval
+
+
+def get_dep_ids(user: dict) -> list[int] | None:
+    """Get department IDs from user data."""
+    if not user or not isinstance(user, dict):
+        return None
+    dep_ids = user.get("UF_DEPARTMENT")
+    if not dep_ids or not isinstance(dep_ids, list):
+        return None
+    return dep_ids
+
+
+def get_dep_id(user: dict) -> int | None:
+    """Get department ID from user data."""
+    if not user or not isinstance(user, dict):
+        return None
+    dep_ids = user.get("UF_DEPARTMENT")
+    if not dep_ids or not isinstance(dep_ids, list):
+        return None
+    return dep_ids[0]
+
+
+def is_fired(user: dict) -> bool | None:
+    if not user or not isinstance(user, dict):
+        return
+    active = user.get("ACTIVE")
+    if active is None:
+        return None
+    return not user.get("ACTIVE")
+
+
+def is_in_prohibited_department(user: dict) -> bool | None:
+    if not user or not isinstance(user, dict):
+        return
+    prohibited_dep_ids_raw = os.getenv("PROHIBITED_DEPARTMENT_IDS")
+    if not prohibited_dep_ids_raw:
+        return None
+    try:
+        prohibited_dep_ids = literal_eval(prohibited_dep_ids_raw)
+    except (ValueError, SyntaxError):
+        logging.error("Invalid PROHIBITED_DEPARTMENT_IDS format.")
+        return None
+    dep_ids = get_dep_ids(user)
+    if dep_ids is None:
+        return None
+    return any(dep_id in prohibited_dep_ids for dep_id in dep_ids)
+    
