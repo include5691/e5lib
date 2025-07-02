91386c65749061da13f9b46b1956ff41b7a939bf
Andrey Pshenitsyn <include5691@gmail.com>
2025-07-03 00:29:54 +0500

    feat(orm): add asyncronous get engine and get_sessionmaker funcs

--- e5lib/orm/__init__.py
+++ e5lib/orm/__init__.py
@@ -1,2 +1,7 @@
 from .user import User
-from .connections import get_engine, get_sessionmaker
+from .connections import (
+    get_engine,
+    get_sessionmaker,
+    get_async_engine,
+    get_async_sessionmaker,
+)
--- e5lib/orm/connections.py
+++ e5lib/orm/connections.py
@@ -1,9 +1,12 @@
 import os
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
 
 engine_ = None
 sessionmaker_ = None
+async_engine_ = None
+async_sessionmaker_ = None
 
 
 def get_engine(pool_size: int | None = None) -> Engine:
@@ -23,3 +26,22 @@ def get_sessionmaker() -> sessionmaker:
     if not sessionmaker_:
         sessionmaker_ = sessionmaker(bind=get_engine())
     return sessionmaker_
+
+
+def get_async_engine(pool_size: int | None = None) -> AsyncEngine:
+    """Create sqlalchemy async engine or return existing"""
+    global async_engine_
+    if not async_engine_:
+        kwargs = {"pool_pre_ping": True}
+        if pool_size is not None:
+            kwargs["pool_size"] = pool_size
+        async_engine_ = create_async_engine(os.getenv("DB_AUTH_URL"), **kwargs)
+    return async_engine_
+
+
+async def get_async_sessionmaker() -> async_sessionmaker:
+    """Create sqlalchemy async SessionMaker or return existing"""
+    global async_sessionmaker_
+    if not async_sessionmaker_:
+        async_sessionmaker_ = async_sessionmaker(bind=get_async_engine())
+    return async_sessionmaker_
