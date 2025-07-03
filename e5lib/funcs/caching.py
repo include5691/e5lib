ace3a14d6f155dbef5386e8be7583afb980bd8e8
Andrey Pshenitsyn <include5691@gmail.com>
2025-07-03 18:25:16 +0500

    feat(funcs/caching): add support for asyncronous funcs in successful_cache

--- e5lib/funcs/caching.py
+++ e5lib/funcs/caching.py
@@ -1,16 +1,47 @@
+import asyncio
+import functools
+from collections import defaultdict
 from cachetools import Cache
 from cachetools.keys import hashkey
 
+
 def successful_cache(cache: Cache):
-    "Caches only not None results"
+    _locks: dict = defaultdict(asyncio.Lock)
+
     def decorator(func):
-        def wrapper(*args, **kwargs):
-            key = hashkey(func, *args, **kwargs)
-            if key in cache:
-                return cache[key]
+        is_async = asyncio.iscoroutinefunction(func)
+
+        @functools.wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            key = hashkey(*args, **kwargs)
+            result = cache.get(key)
+            if result is not None:
+                return result
+
+            lock = _locks[key]
+            async with lock:
+                try:
+                    result = cache.get(key)
+                    if result is not None:
+                        return result
+                    result = await func(*args, **kwargs)
+                    if result is not None:
+                        cache[key] = result
+                    return result
+                finally:
+                    _locks.pop(key, None)
+
+        @functools.wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            key = hashkey(*args, **kwargs)
+            result = cache.get(key)
+            if result is not None:
+                return result
             result = func(*args, **kwargs)
             if result is not None:
                 cache[key] = result
             return result
-        return wrapper
+
+        return async_wrapper if is_async else sync_wrapper
+
     return decorator
