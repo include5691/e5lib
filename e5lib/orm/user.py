4766953487f6d21616e27e14919429c5f48f8c4e
Andrey Pshenitsyn <include5691@gmail.com>
2025-10-01 21:01:27 +0500

    chore(orm/user): update User model with is_fired mark

--- e5lib/orm/user.py
+++ e5lib/orm/user.py
@@ -1,21 +1,33 @@
 import time
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
+
 
 class User:
     """A mixin class to handle user data denormalization in ORM models"""
-    __tablename__ = 'users'
-    
+
+    __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     timestamp = Column(Integer, nullable=False)
     name = Column(String(50))
     dep_id = Column(Integer)
     dep_name = Column(String(50))
+    is_fired = Column(Boolean, server_default="false", nullable=False)
 
-    def __init__(self, id: str | int, name: str, dep_id: str | int, dep_name: str, timestamp: int | float = time.time()):
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        dep_id: int,
+        dep_name: str,
+        timestamp: int | float = time.time(),
+        is_fired: bool = False,
+    ):
         self.id = id
-        self.timestamp = int(timestamp)
         if name:
             self.name = name[:50]
         self.dep_id = dep_id
         if dep_name:
             self.dep_name = dep_name[:50]
+        self.timestamp = int(timestamp)
+        self.is_fired = is_fired
