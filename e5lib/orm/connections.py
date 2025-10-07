0dd0d0512a32e344cdc70b2b259b848c24a0da94
Andrey Pshenitsyn <include5691@gmail.com>
2025-10-07 11:15:00 +0500

    feat(orm/connections): add db url arg to connections funcs

--- e5lib/orm/connections.py
+++ e5lib/orm/connections.py
@@ -11,39 +11,45 @@ async_engine_ = None
 async_sessionmaker_ = None
 
 
-def get_engine(pool_size: int | None = None) -> Engine:
+def get_engine(db_url: str | None = None, pool_size: int | None = None) -> Engine:
     """Create sqlalchemy engine or return existing"""
     global engine_
     if not engine_:
         kwargs = {"pool_pre_ping": True}
         if pool_size is not None:
             kwargs["pool_size"] = pool_size
-        engine_ = create_engine(DB_URL, **kwargs)
+        db_url = db_url or DB_URL
+        engine_ = create_engine(db_url, **kwargs)
     return engine_
 
 
-def get_sessionmaker() -> sessionmaker:
+def get_sessionmaker(db_url: str | None = None) -> sessionmaker:
     """Create sqlalchemy SessionMaker or return existing"""
     global sessionmaker_
     if not sessionmaker_:
-        sessionmaker_ = sessionmaker(bind=get_engine())
+        db_url = db_url or DB_URL
+        sessionmaker_ = sessionmaker(bind=get_engine(db_url))
     return sessionmaker_
 
 
-def get_async_engine(pool_size: int | None = None) -> AsyncEngine:
+def get_async_engine(
+    db_url: str | None = None, pool_size: int | None = None
+) -> AsyncEngine:
     """Create sqlalchemy async engine or return existing"""
     global async_engine_
     if not async_engine_:
         kwargs = {"pool_pre_ping": True}
         if pool_size is not None:
             kwargs["pool_size"] = pool_size
-        async_engine_ = create_async_engine(DB_URL, **kwargs)
+        db_url = db_url or DB_URL
+        async_engine_ = create_async_engine(db_url, **kwargs)
     return async_engine_
 
 
-def get_async_sessionmaker() -> async_sessionmaker:
+def get_async_sessionmaker(db_url: str | None = None) -> async_sessionmaker:
     """Create sqlalchemy async SessionMaker or return existing"""
     global async_sessionmaker_
     if not async_sessionmaker_:
-        async_sessionmaker_ = async_sessionmaker(bind=get_async_engine())
+        db_url = db_url or DB_URL
+        async_sessionmaker_ = async_sessionmaker(bind=get_async_engine(db_url))
     return async_sessionmaker_
